d6d3ba2783f59fbb8519f72b483e343ec27722e3
l_anuranga <l.anuranga@directfn.com>
2019-06-24 17:25:59 +0530

    initial commit of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoServiceService } from './todo-service.service';
+import { HeaderComponent } from './header/header.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+    TodoItemComponent,
+    TodoListComponent,
+    HeaderComponent,
+    AddTodoComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [TodoServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
